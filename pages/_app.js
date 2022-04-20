f41c344dff2775a3f8d0275a2051d77fcfe14e0e
Abraham Serrano <abrahamserrano96@hotmail.com>
2022-04-20 14:45:08 -0500

    🛠  pages index, app and document

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
-
+import { NextUIProvider } from "@nextui-org/react";
+import "../styles/globals.css";
+import { I18NProvider } from "context/i18n.js";
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <I18NProvider>
+        <Component {...pageProps} />
+      </I18NProvider>
+    </NextUIProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
